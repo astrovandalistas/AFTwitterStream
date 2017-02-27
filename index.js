6f910c388d8b576dbd2be1edbb8055dd0a4e4bfb
Thiago Hersan <thiago.hersan+github@gmail.com>
2017-02-27 20:08:32 -0300

    [thiago] adds queue and response functions

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ var app = express();
 
 var queryString = "alienating, hatemyjob, distance relationship";
 
+var insertIndex = 0;
+var popIndex = 0;
+var QUEUE_SIZE = 64;
+var mQueue = [
+  "Unhappily submitting to the Machine destroys your soul not all at once but over time",
+  "Once the realization is accepted that even between the closest human beings infinite distances continue",
+  "We are made aware of our need to assimilate with something far beyond ourselves by our insatiable longing",
+  "I hate my life and my job and my president and my country, but at least star wars 8 is just 10 months away"
+];
+
 var client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -20,6 +30,12 @@ client.stream('statuses/filter', {track: queryString}, function(stream){
   stream.on('data', function(tweet) {
     var mText = tweet.text.replace(/RT /g, "").replace(/["{}<>().…?!,;\-#]/g, "").replace(/@\S+/g, "").replace(/http(s?):\/\/\S+/g, "").replace(/([a-zA-Z]+)\/([a-zA-Z]+)/g, "$1 $2").replace(/distance relationship/ig, "distance").replace(/\s+/g, " ").trim();
 
+    if(mQueue.length < QUEUE_SIZE) {
+      mQueue.push(mText);
+    } else {
+      mQueue[insertIndex] = mText;
+      insertIndex = (insertIndex + 1)%mQueue.length;
+    }
     console.log(mText);
   });
 
@@ -28,5 +44,17 @@ client.stream('statuses/filter', {track: queryString}, function(stream){
   });
 });
 
-//app.get('/AFT', function() {});
+app.get('/AFT', function(req, res) {
+  res.send(mQueue[popIndex]);
+  popIndex = (popIndex + 1)%mQueue.length;
+});
+
+app.get('/AFTALL', function(req, res) {
+  var queueString = "";
+  for(var i=0; i<mQueue.length; i++) {
+    queueString += mQueue[i]+",<br>";
+  }
+  res.send(queueString);
+});
+
 app.listen(port);
